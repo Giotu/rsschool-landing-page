0ba880257a3fa4015928657c4ef0ec37aa31b613
Giotu <evilalex1998@mail.ru>
2026-09-23 07:39:22 +0300

    feat: add toggle theme

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
-const userName = "Giotu";
+const root = document.documentElement;
+const themeToggle = document.querySelector(".theme-toggle");
 
-console.log(userName);
+const sunIcon = document.querySelector(".theme-toggle__icon--sun");
+const moonIcon = document.querySelector(".theme-toggle__icon--moon");
+
+updateThemeIcons(root.dataset.theme);
+
+themeToggle.addEventListener("click", () => {
+  const currentTheme = root.dataset.theme;
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+
+  root.dataset.theme = newTheme;
+  localStorage.setItem("theme", newTheme);
+
+  updateThemeIcons(newTheme);
+});
+
+function updateThemeIcons(theme) {
+  sunIcon.classList.toggle("theme-toggle__icon--active", theme === "light");
+
+  moonIcon.classList.toggle("theme-toggle__icon--active", theme === "dark");
+}
